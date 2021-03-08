17a8fee0c636294be34e27f5c7071c2df8da9030
wuyurong <yuk_wing@163.com>
2021-03-08 15:42:53 +0800

    二叉树

---
+++ src/BinaryTree/Leetcode297.java
@@ -0,0 +1,40 @@
+package BinaryTree;
+
+public class Leetcode297 {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return resialize(root,"");
+    }
+
+    public String resialize(TreeNode root,String str){
+        if (root==null){
+            return str+"None,";
+        }else {
+            str+=root.val+",";
+            resialize(root.left,str);
+            resialize(root.right,str);
+        }
+        return str;
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode rdeserialize(List<String> l) {
+        if (l.get(0).equals("None")) {
+            l.remove(0);
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.valueOf(l.get(0)));
+        l.remove(0);
+        root.left = rdeserialize(l);
+        root.right = rdeserialize(l);
+
+        return root;
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] data_array = data.split(",");
+        List<String> data_list = new LinkedList<String>(Arrays.asList(data_array));
+        return rdeserialize(data_list);
+    }
+
+}
